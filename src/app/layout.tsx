83ac664e71a81aaebe45aa9f4eb23c25e885e7df
Dave Edwards <dave@dwedigital.com>
2025-12-15 17:58:55 +0000

    added speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 
 
 const geistSans = Geist({
@@ -31,6 +33,7 @@ export default function RootLayout({
       >
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
